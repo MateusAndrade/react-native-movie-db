298e4205a282f8bd1c51b64c426588a41040d6b5
MateusAndrade <mateus.andrade47@outlook.com>
2019-07-20 18:23:56 -0300

    feat(logo): adding basic logo to status bar

---
+++ src/components/Logos/interfaces/logo.ts
@@ -0,0 +1,6 @@
+import { ImageProps } from 'react-native';
+
+export interface ILogoProps extends Partial<ImageProps> {
+  width: number;
+  height: number;
+}
--- src/components/StatusBar/SearchBar/index.tsx
+++ src/components/StatusBar/SearchBar/index.tsx
@@ -1,17 +1,30 @@
 import React, { FunctionComponent } from 'react';
 
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import { IStatusBarProps } from '../interfaces/status-bar';
+
+import { StackedLogo } from '../../Logos';
 
 interface ISearchBarProps extends IStatusBarProps {}
 
 const SearchBar: FunctionComponent<ISearchBarProps> = () => {
   return (
-    <View>
+    <View style={[styles.container, styles.row]}>
+      <StackedLogo height={30} width={40} resizeMode="contain" />
       <Text>SearchBar</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    height: 50,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+});
 
 export default SearchBar;
