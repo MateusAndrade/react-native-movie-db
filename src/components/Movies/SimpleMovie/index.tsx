b23abcd1c3ee761f66992dc8bbb966fac3ab739e
MateusAndrade <mateus.andrade47@outlook.com>
2019-07-21 00:59:52 -0300

    feat(animation): adding animations

--- src/components/Movies/SimpleMovie/index.tsx
+++ src/components/Movies/SimpleMovie/index.tsx
@@ -6,6 +6,8 @@ import Config from 'react-native-config';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
+import { View as AnimatedView } from 'react-native-animatable';
+
 import { IDiscoverDomain } from '../../../../sdk/discover/discover.interface';
 
 import colors from '../../../constants/colors';
@@ -18,21 +20,24 @@ const SimpleMovieBox: FunctionComponent<ISimpleMovieBox> = props => {
   const { onSelectMovie, poster_path, vote_average } = props;
 
   return (
-    <TouchableOpacity
-      onPress={() => onSelectMovie(props)}
-      style={styles.container}
-    >
-      <Image
-        style={styles.moviePoster}
-        source={{ uri: `${Config.MOVIE_DB_IMG_URL}/w500/${poster_path}` }}
-      />
-      <View style={styles.movieInfo}>
-        <Icon name="ios-star" size={15} color={colors.WHITE} />
-        <Text style={styles.popularity} ellipsizeMode="tail" numberOfLines={1}>
-          {vote_average}
-        </Text>
-      </View>
-    </TouchableOpacity>
+    <AnimatedView animation="fadeInDownBig" style={styles.container}>
+      <TouchableOpacity onPress={() => onSelectMovie(props)}>
+        <Image
+          style={styles.moviePoster}
+          source={{ uri: `${Config.MOVIE_DB_IMG_URL}/w500/${poster_path}` }}
+        />
+        <View style={styles.movieInfo}>
+          <Icon name="ios-star" size={15} color={colors.WHITE} />
+          <Text
+            style={styles.popularity}
+            ellipsizeMode="tail"
+            numberOfLines={1}
+          >
+            {vote_average}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </AnimatedView>
   );
 };
 
